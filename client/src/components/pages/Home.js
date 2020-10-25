e54db65160d26a496a396f9c8ec7a71bd861e941
ofsen <ofsensmailbox@gmail.com>
2020-10-25 09:45:49 +0100

    Filter Contacts

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
 
 const Home = () => {
 	return (
@@ -9,6 +10,8 @@ const Home = () => {
 				<ContactForm />
 			</div>
 			<div>
+				<h2 className='text-primary'>Contacts List</h2>
+				<ContactFilter />
 				<Contacts />
 			</div>
 		</div>
